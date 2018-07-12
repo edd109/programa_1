1fc18ae90b6654df23f0e97246841b054f8fab24
EDD <eduardo_constante_109@hotmail.com>
2018-07-12 14:40:01 -0500

    adivinar

---
+++ adivinar.py
@@ -0,0 +1,12 @@
+
+number_to_guess = 2
+
+numero_ingresado = int(input("adivina el numero: "))
+
+
+
+if number_to_guess ==   numero_ingresado:
+    print ("adivinaste ")
+
+else:
+    print ("perdiste")
