8e503ed3d33292c9398a8d8b2a064b9845d70770
EDD <eduardo_constante_109@hotmail.com>
2018-07-16 14:37:03 -0500

    adivinar

---
+++ recopilacion.py
@@ -0,0 +1,18 @@
+print ("Bienvenido Imbsesil")
+
+Ingreso = input("Vas  a entrar a la Heladeria? (SI/NO)")
+
+if Ingreso == "SI":
+    print("BIENVENIDO")
+    Ordenar = input("Deseas ordenar  algo? (SI/NO)")
+    if Ordenar == "SI":
+            tener_dinero = input("Tienes Dinero? (SI/NO)")
+
+            if tener_dinero == "SI":
+                 dinero = int(input("Cuanto dinero dispones?"))
+                 print ("De acuerdo este es el menu que tenemos ")
+
+
+
+
+
